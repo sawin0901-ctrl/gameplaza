736e626c91d6f94d2a040d66e094c9316160905f
sawin0901-ctrl <sawin0901@gmail.com>
2026-06-19 17:31:38 +1000

    fix: src/components/DigisellerScript.tsx

--- src/components/DigisellerScript.tsx
+++ src/components/DigisellerScript.tsx
@@ -4,25 +4,22 @@ import { usePathname } from "next/navigation"
 
 const SELLER_ID = "1459731"
 
-const DIGISELLER_SCRIPT = `!function(e){var l=function(l){return e.cookie.match(new RegExp("(?:^|; )digiseller-"+l+"=([^;]*)"))},i=l("lang"),s=l("cart_uid"),t=i?"&lang="+i[1]:"",d=s?"&cart_uid="+s[1]:"",r=e.getElementsByTagName("head")[0]||e.documentElement,n=e.createElement("link"),a=e.createElement("script");n.type="text/css",n.rel="stylesheet",n.id="digiseller-css",n.href="//shop.digiseller.com/xml/store2_css.asp?seller_id=${SELLER_ID}",a.async=!0,a.id="digiseller-js",a.src="//digiseller.com/store2/digiseller-api.js.asp?seller_id=${SELLER_ID}"+t+d,!e.getElementById(n.id)&&r.appendChild(n),!e.getElementById(a.id)&&r.appendChild(a)}(document);`
-
 export default function DigisellerScript() {
   const pathname = usePathname()
+  const isProductPage = pathname.startsWith("/product/")
 
   useEffect(() => {
-    // Remove old script to force re-init on navigation
-    const old = document.getElementById("digiseller-js")
-    if (old) old.remove()
+    if (!isProductPage) return
+
+    const inlineId = "digiseller-inline"
+    document.getElementById(inlineId)?.remove()
+    document.getElementById("digiseller-js")?.remove()
 
     const el = document.createElement("script")
-    el.id = "digiseller-js-inline"
-    el.textContent = DIGISELLER_SCRIPT
+    el.id = inlineId
+    el.textContent = `!function(e){var l=function(l){return e.cookie.match(new RegExp("(?:^|; )digiseller-"+l+"=([^;]*)"))},i=l("lang"),s=l("cart_uid"),t=i?"&lang="+i[1]:"",d=s?"&cart_uid="+s[1]:"",r=e.getElementsByTagName("head")[0]||e.documentElement,n=e.createElement("link"),a=e.createElement("script");n.type="text/css",n.rel="stylesheet",n.id="digiseller-css",n.href="//shop.digiseller.com/xml/store2_css.asp?seller_id=${SELLER_ID}",a.async=!0,a.id="digiseller-js",a.src="//digiseller.com/store2/digiseller-api.js.asp?seller_id=${SELLER_ID}"+t+d,!e.getElementById(n.id)&&r.appendChild(n),!e.getElementById(a.id)&&r.appendChild(a)}(document);`
     document.head.appendChild(el)
-
-    return () => {
-      document.getElementById("digiseller-js-inline")?.remove()
-    }
-  }, [pathname])
+  }, [pathname, isProductPage])
 
   return null
 }
