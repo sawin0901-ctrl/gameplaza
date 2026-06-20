09c2e12807ca03d5e3df6c4964cb0a83f0f83be2
sawin0901-ctrl <sawin0901@gmail.com>
2026-06-20 12:28:50 +1000

    feat: add Sentry server config

---
+++ sentry.server.config.ts
@@ -0,0 +1,11 @@
+import * as Sentry from "@sentry/nextjs"
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 0.1,
+  environment: process.env.NODE_ENV,
+  ignoreErrors: [
+    "NEXT_NOT_FOUND",
+    "NEXT_REDIRECT",
+  ],
+})
