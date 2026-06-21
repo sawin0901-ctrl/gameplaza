b58e6ffed1b0595602064ea8f33b0da6dedc6315
sawin0901-ctrl <sawin0901@gmail.com>
2026-06-21 15:36:56 +1000

    fix(sanitize): add table, blockquote, code, colspan/rowspan attrs

--- src/lib/sanitize.ts
+++ src/lib/sanitize.ts
@@ -4,12 +4,18 @@ export function sanitizeDescription(html: string): string {
   return sanitizeHtml(html, {
     allowedTags: [
       "p", "br", "b", "i", "em", "strong", "ul", "ol", "li",
-      "h2", "h3", "h4", "a", "span", "div",
+      "h2", "h3", "h4", "h5", "h6", "a", "span", "div",
+      "table", "thead", "tbody", "tr", "th", "td",
+      "blockquote", "pre", "code", "hr",
     ],
     allowedAttributes: {
-      a: ["href", "title"],
-      span: ["class"],
-      div: ["class"],
+      a: ["href", "title", "target", "rel"],
+      span: ["class", "style"],
+      div: ["class", "style"],
+      table: ["class", "style"],
+      th: ["class", "style", "colspan", "rowspan"],
+      td: ["class", "style", "colspan", "rowspan"],
+      p: ["style"],
     },
     allowedSchemes: ["https", "http"],
   })
